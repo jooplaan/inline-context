fa04ec654eecb03f2945ff4470bf788c04025dc9
Joop Laan <joop.laan@gmail.com>
2025-10-31 17:09:32 +0100

    Accessibility updates

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-components', 'wp-element', 'wp-i18n', 'wp-rich-text'), 'version' => '7ade8b796c624cad0833');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-components', 'wp-element', 'wp-i18n', 'wp-rich-text'), 'version' => 'd2ba157dddf286e4bb40');
