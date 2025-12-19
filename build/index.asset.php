30c3c9982f7d51246dc2efa86bb314ac4392491a
Joop Laan <joop.laan@gmail.com>
2025-12-19 06:30:23 +0100

    Let Quill handle active state styling (blue text, no background)

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-block-editor', 'wp-components', 'wp-data', 'wp-editor', 'wp-element', 'wp-i18n', 'wp-plugins', 'wp-rich-text'), 'version' => '9bbb9d0117a22f194b46');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-block-editor', 'wp-components', 'wp-data', 'wp-editor', 'wp-element', 'wp-i18n', 'wp-plugins', 'wp-rich-text'), 'version' => '860421c01e7baf229b1d');
